8422d27761983ba90808573f5317c354faaa25d9
Pratik Thanki <pratikthanki1@gmail.com>
2020-09-14 20:04:09 +0100

    Remove shared

---
+++ NFL.BigDataBowl/Requester.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace NFL.BigDataBowl
+{
+    public class Requester
+    {
+        public static async Task<string> GetData(string url)
+        {
+            var request = WebRequest.Create(url);
+
+            request.ContentType = "application/json";
+            request.Headers.Add("User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36");
+
+            using var response = (HttpWebResponse) await request.GetResponseAsync();
+            await using var dataStream = response.GetResponseStream();
+
+            if (dataStream == null)
+                return null;
+
+            using var reader = new StreamReader(dataStream);
+            var content = await reader.ReadToEndAsync();
+            return content;
+        }
+    }
+}
--- NFL.Combine/CombineCollector.cs
+++ NFL.Combine/CombineCollector.cs
@@ -1,132 +1,127 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
-using NFL.Shared;
 
 namespace NFL.Combine
 {
     public class CombineCollector
     {
-        private readonly string _baseurl;
-        private readonly string[] _workouts;
-        private readonly int[] _seasons;
-        public List<WorkoutResult> Results;
-        private Requester _requester;
-        private FileWriter _fileWriter;
-        private readonly string _combineStats;
+        private readonly List<WorkoutResult> Results;
+        private readonly FileWriter _fileWriter;
+
+        private const string BaseUrl = @"http://www.nfl.com/liveupdate/combine/{0}/{1}/ALL.json";
+        private readonly int[] _seasons = {2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020};
+        private const string CombineStats = "CombineStats";
+
+        private readonly string[] _workouts =
+        {
+            "FORTY_YARD_DASH", "BENCH_PRESS", "VERTICAL_JUMP",
+            "BROAD_JUMP", "THREE_CONE_DRILL", "TWENTY_YARD_SHUTTLE", "SIXTY_YARD_SHUTTLE"
+        };
 
         public CombineCollector()
         {
-            _baseurl = @"http://www.nfl.com/liveupdate/combine/{0}/{1}/ALL.json";
-            _workouts = new[]
-            {
-                "FORTY_YARD_DASH", "BENCH_PRESS", "VERTICAL_JUMP",
-                "BROAD_JUMP", "THREE_CONE_DRILL", "TWENTY_YARD_SHUTTLE", "SIXTY_YARD_SHUTTLE"
-            };
-            _seasons = new[] {2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020};
-
             Results = new List<WorkoutResult>();
-            _requester = new Requester();
-            _combineStats = "CombineStats";
-            _fileWriter = new FileWriter(_combineStats);
+            _fileWriter = new FileWriter(CombineStats);
         }
 
-        private async Task CombineWorkout(int season, string workout)
+        private async Task GetCombineWorkout(int season, string workout)
         {
-            string url = string.Format(_baseurl, season, workout);
+            var url = string.Format(BaseUrl, season, workout);
 
-            var response = await _requester.GetData(url);
-            CombineRootObject responseJson = JsonConvert.DeserializeObject<CombineRootObject>(response);
+            var response = await GetData(url);
+            var responseJson = JsonConvert.DeserializeObject<CombineRootObject>(response);
 
-            foreach (CombineWorkout row in responseJson.Data)
+            foreach (var row in responseJson.Data.Where(row => row != null))
             {
-                if (row == null)
-                {
-                    continue;
-                }
-
                 if (!Results.Exists(x => x.Id == row.Id))
-                {
                     Results.Add(new WorkoutResult(row, season));
-                }
 
                 UpsertCombineStat(row);
             }
 
-            _fileWriter.WriteToFile(Results.FindAll(x => x.Season == season), $"{_combineStats}{season}");
+            _fileWriter.WriteToFile(
+                Results.FindAll(x => x.Season == season), $"{CombineStats}{season}");
         }
 
         private void UpsertCombineStat(CombineWorkout row)
         {
-            string workoutName = row.WorkoutName;
+            var workoutName = row.WorkoutName;
             float? result = null;
+
             if (row.Result != null)
-            {
                 result = float.Parse(row.Result);
-            }
 
             foreach (var item in Results.Where(r => r.Id == row.Id))
             {
-                if (workoutName == WorkoutNames.FortyYardDash)
-                {
+                if (workoutName == Workouts.FortyYardDash)
                     item.FortyYardDash = result;
-                }
-                else if (workoutName == WorkoutNames.BenchPress)
-                {
+
+                else if (workoutName == Workouts.BenchPress)
                     item.BenchPress = result;
-                }
-                else if (workoutName == WorkoutNames.VerticalJump)
-                {
+
+                else if (workoutName == Workouts.VerticalJump)
                     item.VerticalJump = result;
-                }
-                else if (workoutName == WorkoutNames.BroadJump)
-                {
+
+                else if (workoutName == Workouts.BroadJump)
                     item.BroadJump = result;
-                }
-                else if (workoutName == WorkoutNames.ThreeConeDrill)
-                {
+
+                else if (workoutName == Workouts.ThreeConeDrill)
                     item.ThreeConeDrill = result;
-                }
-                else if (workoutName == WorkoutNames.TwentyYardShuttle)
-                {
+
+                else if (workoutName == Workouts.TwentyYardShuttle)
                     item.TwentyYardShuttle = result;
-                }
-                else if (workoutName == WorkoutNames.SixtyYardShuttle)
-                {
+
+                else if (workoutName == Workouts.SixtyYardShuttle)
                     item.SixtyYardShuttle = result;
-                }
+
                 else
-                {
                     Console.WriteLine($"Invalid workout: PlayerId: {item.Id}; WorkoutName: {workoutName}");
-                }
             }
         }
 
         private void PrintResults()
         {
             foreach (var result in Results)
-            {
                 Console.WriteLine(result.ToString());
-            }
         }
 
         public async Task AllCombineWorkouts(int season)
         {
-            foreach(var workout in _workouts)
+            foreach (var workout in _workouts)
             {
                 Console.WriteLine($"Season: {season}, Workout: {workout}");
-                await CombineWorkout(season, workout);
+                await GetCombineWorkout(season, workout);
             }
         }
 
         public async Task BackloadCombineWorkouts()
         {
             foreach (var season in _seasons)
-            {
                 await AllCombineWorkouts(season);
-            }
+        }
+
+        private static async Task<string> GetData(string url)
+        {
+            var request = WebRequest.Create(url);
+
+            request.ContentType = "application/json";
+            request.Headers.Add("User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36");
+
+            using var response = (HttpWebResponse) await request.GetResponseAsync();
+            await using var dataStream = response.GetResponseStream();
+
+            if (dataStream == null)
+                return null;
+
+            using var reader = new StreamReader(dataStream);
+            var content = await reader.ReadToEndAsync();
+            return content;
         }
     }
 }
