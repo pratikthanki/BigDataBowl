edc2bfd34c303fea05125c0ab7c29c6720939d12
Pratik Thanki <pratikthanki1@gmail.com>
2020-09-15 23:32:57 +0100

    refactor method names

--- NFL.BigDataBowl/Utilities/StringParser.cs
+++ NFL.BigDataBowl/Utilities/StringParser.cs
@@ -2,16 +2,16 @@ using System;
 
 namespace NFL.BigDataBowl.Utilities
 {
-    public static class InputHandler
+    public static class StringParser
     {
-        public static float ParseFloat(string number) =>
+        public static float ToFloat(string number) =>
             (float) (number == "" || number == "0" ? 0.0 : float.Parse(number));
 
-        public static int ParseInt(string number) => number == "" ? 0 : int.Parse(number);
+        public static int ToInt(string number) => number == "" ? 0 : int.Parse(number);
 
-        public static long ParseLong(string number) => long.Parse(number);
+        public static long ToLong(string number) => long.Parse(number);
 
-        public static DateTime ParseDateTime(string number, string format) =>
-            DateTime.ParseExact(number, format, null);
+        public static DateTime ToDateTime(string dateTime, string format) =>
+            DateTime.ParseExact(dateTime, format, null);
     }
 }
